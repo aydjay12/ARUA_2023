a262d2c274f4a76961e66b3acd00c8f5f5c5e47b
Oluwapelumi-Israel <israeloluwapelumi@gmail.com>
2023-09-21 23:36:24 +0100

    Updated on postal sesion by pelumi

--- src/components/Livepodcast/Livepostalcard.js
+++ src/components/Livepodcast/Livepostalcard.js
@@ -1,51 +1,14 @@
-import { useNewContext } from "../../Context/NewContext";
+
 import '../../styles/Stylelivepodcast/Livepostal.css';
-import { useRef } from "react";
+
 
 
 
 
 
 let Livepostalcard = () => {
-    const {Image} = useNewContext();
-    const sliderRef = useRef(null);
-
-    let len = Image.album.length;
-
-    let counter = 0;
-
-   
-
-    let loadPrev = () => {
-        counter = counter > 0? counter - 1 : 0;
-        sliderRef.current.style.transform = `translate(-${counter * 25}%)`;
-        sliderRef.current.style.transition = 'transform 1s ease-in';
-    }
-
-    let loadNext = () => {
-        counter = counter < len - 1 ? counter + 1 : len - 1;
-        sliderRef.current.style.transform = `translate(-${counter * 25}%)`;
-        sliderRef.current.style.transition = 'transform 1s ease-in';   
-    }
-
     return (
-        <section className="livepostalsession">
-            <section className="carousel_container">
-                <section className="carousel">
-                    <section className="slider" ref={sliderRef}>
-                            <img src={Image.album[0]} alt=""></img>
-                            <img src={Image.album[1]} alt=""></img>
-                            <img src={Image.album[2]} alt=""></img>
-                            <img src={Image.album[3]} alt=""></img>
-                    </section>
-                </section>
-                <section className="arrow prev" onClick={loadPrev}>Prev</section>
-                <section className="arrow next" onClick={loadNext}>Next</section>
-            </section>
-            <section className="livechat">
-                
-            </section>
-        </section>
+        <section style={{height: '800px', letterSpacing: '10px', textAlign: 'center', marginTop: '3em'}}>POSTAL PRESENTATION SESSION</section>
     );
 };
 
--- src/components/Livepodcast/Redirecttolivepodcast.js
+++ src/components/Livepodcast/Redirecttolivepodcast.js
@@ -1,42 +1,13 @@
 import {Link} from 'react-router-dom';
-import '../../styles/Stylelivepodcast/Redirect.css'
-import thumbnailone from '../../assets/thumbnail-1.jpg';
-import thumbnailtwo from '../../assets/thumbnail-2.jpg';
-import thumbnailthree from '../../assets/thumbnail-3.jpg';
-import thumbnailfour from '../../assets/thumbnail-4.jpg';
-import { useNewContext } from '../../Context/NewContext';
+import '../../styles/Stylelivepodcast/Redirect.css';
+
 
 
 export let Redirect = () => {
 
-    const {Image , setImage} = useNewContext();
-
-    
-    
     return (
         <section className="Container">
-                <section className="Slider">
-                    <section>
-                        <Link to='/livepostalcard'><img src={thumbnailone} alt='first_image' onClick={() => {
-                            setImage({...Image, album : [thumbnailone , thumbnailtwo , thumbnailthree , thumbnailfour], img: thumbnailone});
-                        }}></img></Link>
-                    </section>
-                    <section>
-                        <Link to='/livepostalcard'><img src={thumbnailtwo} alt='first_image' onClick={() => {
-                            setImage({...Image, album : [thumbnailone , thumbnailtwo , thumbnailthree , thumbnailfour] , img: thumbnailtwo});
-                        }}></img></Link>
-                    </section>
-                    <section>
-                        <Link to='/livepostalcard'><img src={thumbnailthree} alt='first_image' onClick={() => {
-                            setImage({...Image, album : [thumbnailone , thumbnailtwo , thumbnailthree , thumbnailfour] , img: thumbnailthree});
-                        }}></img></Link>
-                    </section>
-                    <section>
-                        <Link to='/livepostalcard'><img src={thumbnailfour} alt='first_image' onClick={() => {
-                            setImage({...Image, album : [thumbnailone , thumbnailtwo , thumbnailthree , thumbnailfour] , img: thumbnailfour});
-                        }}></img></Link>
-                    </section>
-                </section>   
+            <section className='Link'><Link to="/livepostalcard">Click here to view postal presentation</Link></section>
         </section>
     );
 };
